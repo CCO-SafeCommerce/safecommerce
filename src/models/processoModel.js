7c4f41bc376dec452cca732574ca34c4f51f060d
PedroHFD13 <pedro.dias@sptech.school>
2022-12-03 21:21:30 -0300

    Tela de processos 2.0

--- src/models/processoModel.js
+++ src/models/processoModel.js
@@ -3,7 +3,7 @@ const database = require('../database/config')
 function obterProcessos(fkServidor) {
     console.log("ACESSEI O Processo MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function obterProcessos(): ")
     var instrucao = `
-        select TOP(10) pid, dataLeitura, nome, usoCpu, usoRam from Processo where fkServidor = '${fkServidor}' order by dataLeitura desc, usoCpu desc;
+        select TOP(10) pid, nome, usoCpu, usoRam from Processo where fkServidor = '${fkServidor}' order by dataLeitura desc, usoCpu desc;
     `
 
     return database.execute(instrucao)
